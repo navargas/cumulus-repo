cea292e9fcc74b9d86f9215e97d92ed0810e49db
Nicholas Vargas <mail@nickvargas.net>
2016-03-09 16:25:04 -0800

    Implement "get all assets" and "add new asset"

--- routes/assets.js
+++ routes/assets.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var express = require('express');
 var sqlite = require('sqlite3');
 var auth = require('../lib/auth.js');
@@ -7,15 +8,40 @@ var router = express.Router();
 var conf = null;
 var db = null;
 
+/* SQL Statements */
+var SQL_GET_ASSETS_NO_PERMISSIONS = 'SELECT name, owner, description ' +
+                                    'FROM assets LIMIT 50;';
+var SQL_NEW_ASSET = 'INSERT INTO assets (name, owner, description) ' +
+                    'VALUES (?, ?, ?);';
+
 router.get('/', function(req, res) {
-  res.send('{"index":"yes"}');
+  db.all(SQL_GET_ASSETS_NO_PERMISSIONS, function (err, data) {
+    if (err) {
+      console.error(err.toString());
+      res.send({error:err.toString()});
+    } else {
+      res.send(data);
+    }
+  });
 });
 
 router.put('/:assetName', auth.verify, function(req, res) {
-  res.send({'_request-for':req.authenticatedUser});
+  /* Add new asset named assetName */
+  var description = req.body.desc || '';
+  var newAsset = db.prepare(SQL_NEW_ASSET);
+  var record = [req.params.assetName, req.authenticatedUser, description];
+  newAsset.run(record, function(err, data) {
+    if (err) {
+      console.error(err.toString());
+      res.send({error:err.toString()});
+    } else {
+      res.send({owner:record[1], name:record[0]});
+    }
+  });
 });
 
 exports.init = function(configuration) {
   conf = configuration;
+  db = new sqlite.Database(path.join(conf.storageDir, conf.dbFileName));
   return router;
 }
