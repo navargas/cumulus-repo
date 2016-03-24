0a2cf60f2f226d13759b7a4388901c3ddddc8307
Nick Vargas <mail@nickvargas.net>
2016-03-24 13:31:28 -0700

    Add "get all groups" target

--- routes/groups.js
+++ routes/groups.js
@@ -9,12 +9,18 @@ var conf = null;
 var db = null;
 
 var SQL_ADD_NEW_GROUP = 'INSERT INTO groups (name, owner) VALUES (?, ?)';
+var SQL_GET_GROUPS = 'SELECT name, owner FROM groups LIMIT 50';
 
 router.get('/', function(req, res) {
-  res.send('{"index":"yes"}');
+  /* Return a list of all groups (limit 50) */
+  db.all(SQL_GET_GROUPS, function(err, data) {
+    if (err) return res.status(500).send({error: err.toString()});
+    res.send(data);
+  });
 });
 
 router.put('/:groupName', auth.verify, function(req, res) {
+  /* Create a new group */
   var name = req.params.groupName;
   var owner = req.authenticatedUser;
   if (!conf.validName.test(name)) {
